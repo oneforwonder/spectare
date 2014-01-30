4bcb9e56f4ba9c113ca441bb8b3b7bdcd589a70d
Rob Nagle <rjn945@gmail.com>
2014-01-30 15:31:17 -0800

    Parellel lines can now fade + a few fixes

--- src/spectare/parallel.clj
+++ src/spectare/parallel.clj
@@ -2,12 +2,25 @@
   (:use spectare.util
         quil.core)) 
 
-(def WIDTH (screen-width))
+(def WIDTH (min (screen-width) 1920))
 (def HEIGHT (screen-height))
 
-(def FPS 1)
+(def FPS 60)
 
+(def AVG-LINE-COLL-COUNT 30)
 (def LINE-THICKNESS 2)
+(def LINES-FADE true)
+(def LINES-ON-SCREEN 100) ; Only used if lines fade
+
+(def frame-num (atom 0))
+
+;; Helpers
+(defn on-screen? [line]
+  (let [{:keys [x1 y1 x2 y2]} line]
+    (or (and (< 0 x1 WIDTH)
+             (< 0 y1 HEIGHT))
+        (and (< 0 x2 WIDTH)
+             (< 0 y2 HEIGHT)))))
 
 (defn rand-pos []
   [(rand-int WIDTH) (rand-int HEIGHT)])
@@ -25,28 +38,54 @@
         c2 (rand-color)
         p1 (rand-pos)
         p2 (rand-pos)
-        dx (+- (rand-int 5))
-        dy (+- (rand-int 5))
-        n  (+ 60 (rand-int 100))]
-    (map #(make-line c1 c2 p1 p2 dx dy % n) (range n))))
+        dt (brand-int 5 10)
+        dx (+- (rand-int dt))
+        dy (+- (- dt dx))
+        ;dy (+- (- dt (abs dx)))
+        ac (* 0.67 AVG-LINE-COLL-COUNT)
+        n  (+ ac (rand-int ac))] ; avg +/- a third
+    (filter on-screen? (map #(make-line c1 c2 p1 p2 dx dy % n) (range n)))))
 
 ;; Quil fns
 (defn setup []
+  (def lines (apply concat (repeatedly make-lines)))
+  (background 0 0 0)
   (smooth)
   (frame-rate FPS))
 
-(defn draw! [lines]
-  (stroke-weight LINE-THICKNESS)
-  (doseq [{:keys [x1 y1 x2 y2 col]} lines]
-    (apply stroke col)
+(defn draw-line! [{:keys [x1 y1 x2 y2 col]} alpha] 
+  (let [c* (if (= alpha 200) 
+             [255 255 255] 
+             (map #(* % (/ alpha 255)) col))]
+    (when (= alpha 199)
+      (println "Col:" col)
+      (println "C*:" c*))
+    (stroke-weight LINE-THICKNESS)
+    (apply stroke c*)
     (line x1 y1 x2 y2)))
 
-(defn update []
-  (draw! (make-lines)))
+(defn draw-bg! []
+  (no-stroke)
+  (fill 0 0 0)
+  (rect 0 0 WIDTH HEIGHT))
+
+(defn update-fading []
+  (let [i  (inc @frame-num)
+        d  (max 0 (- i LINES-ON-SCREEN))
+        ls (drop d (take i lines))
+        l* (map vector (reverse ls) (iterate #(- % (/ 255 LINES-ON-SCREEN)) 255))]
+    (draw-bg!)
+    (doseq [[l a] (reverse l*)]
+      (draw-line! l a))
+    (swap! frame-num inc)))
+
+(defn update-stacking []
+  (draw-line! (nth lines @frame-num) 255)
+  (swap! frame-num inc))
 
 (defn -main []
   (defsketch parallel
     :title "parallel"
     :setup setup
-    :draw update
+    :draw (if LINES-FADE update-fading update-stacking)
     :size [WIDTH HEIGHT]))
--- src/spectare/util.clj
+++ src/spectare/util.clj
@@ -1,8 +1,8 @@
 (ns spectare.util)
 
-; Rand fns
+; Random fns
 (defn brand-int 
-  "Returns a bounded random integer"
+  "Returns a bounded random integer in range [x,y)"
   [x y]
   (+ x (rand-int (- y x))))
 
@@ -22,5 +22,12 @@
 (defn gradient 
   "percent=0.0 -> c1; percent=1.0 -> c2; 0.0 < percent < 1.0 -> a blend"
   [c1 c2 percent]
-  (map (fn [cc1 cc2] (+ cc1 (* percent (- cc2 cc1)))) c1 c2))
+  (map (fn [cc1 cc2] (+ 0.0 cc1 (* percent (- cc2 cc1)))) c1 c2))
 
+;; Misc fns
+(defn indexed
+    "Returns a lazy sequence of [index, item] pairs, where items come
+    from 's' and indexes count up from zero.
+      (indexed '(a b c d))  =>  ([0 a] [1 b] [2 c] [3 d])"
+    [s]
+    (map vector  (iterate inc 0) s))
